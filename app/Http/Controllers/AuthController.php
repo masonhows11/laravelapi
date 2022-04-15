2f0bdeb120806fe50dbc7dc900849b8ed292b07b
mason.hows11 <mason.hows11@gmail.com>
2022-04-15 12:38:24 +0430

    reg user updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => ['required', 'min:2', 'max:20'],
+            'email' => ['required', 'email','unique:users','string'],
+            'password' => ['required','confirmed','min:2','max:8']
+        ]);
+
+        $user = User::create([
+           'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
     }
 
