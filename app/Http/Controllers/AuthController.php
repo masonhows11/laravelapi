6cd4a458cdb44651b43ed1716e6d0771d3867614
mason.hows11 <mason.hows11@gmail.com>
2022-04-15 12:32:08 +0430

    add user auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+}
